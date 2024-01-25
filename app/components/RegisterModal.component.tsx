1fc557def4d2a8dd8be8549be721db9a3e446807
Sheriff <129645333+stDean@users.noreply.github.com>
2024-01-25 15:48:42 +0100

    Update RegisterModal.component.tsx

--- app/components/RegisterModal.component.tsx
+++ app/components/RegisterModal.component.tsx
@@ -89,12 +89,12 @@ const RegisterModal = () => {
     <div className="flex flex-col gap-4 mt-3">
       <hr />
 
-      <Button
+{/*       <Button
         outline
         label="Continue with Google"
         icon={FcGoogle}
         onClick={() => signIn("google")}
-      />
+      /> */}
       <Button
         outline
         label="Continue with Github"
